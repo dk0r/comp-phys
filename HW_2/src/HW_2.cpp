8c59158a393b5f947980e3513355397cc9a07e9e
dk0r <dk0r.public@gmail.com>
2013-02-26 21:57:33 -0500

    Code added to monteCarlo5Dn() which generates a csv with "relError,N(number of points)". Useful for plotting (question#2).
    Also added totalRands() which determines the number of rand()'s used in
    monteCarlo5Dn(). Useful to determine when to srand(0) (reseed).  
    I srand(0) monteCarlo5Dn() when it used more than "RAND_MAX" random
    numbers but this had very little (if any) affect on the "Rel Error vs. N
    points" plot.

--- HW_2/src/HW_2.cpp
+++ HW_2/src/HW_2.cpp
@@ -119,7 +119,7 @@ double defaultPRNG()
 	 double u1=1,u2=1,u3=1,u4=1,u5=1;
 	 double l1=0,l2=0,l3=0,l4=0,l5=0;
 
-	 double N = 4000;
+	 double N = 90000;
 	 double coeff5D = ( (u1-l1)*(u2-l2)*(u3-l3)*(u4-l4)*(u5-l5) ) / N;
 	 cout << "coeff5D"
 			 "=" << coeff5D << endl;
@@ -149,8 +149,104 @@ double defaultPRNG()
 	 return 0;
  }
 
+ double totalRands(int N)
+ {
+	 double sum = 0;
+
+
+	 for(int i=N; i>=2; i--)
+	 {
+		 //cout << "i = " << i << endl;
+		 sum += i;
+	 }
+	 //cout << "Sum of N's = " << sum << endl;
+
+	 double total = 5*(1+sum);
+
+	 return total;
+ }
+
+
+ double monteCarlo5Dn()
+ {
+	 cout << "RAND_MAX = " << RAND_MAX << endl;
+	 double u1=1,u2=1,u3=1,u4=1,u5=1;
+	 double l1=0,l2=0,l3=0,l4=0,l5=0;
+	 double N = 50000;
+
+	 ofstream of;
+	 of.open("monteCarlo5Dn.csv");
+	 of << "N,relError" << endl;
+
+	 for(int i=1; i<=N; i++)
+	 {
+		 if (i%5000 == 0)
+		 {cout << " i = " << i << endl;}
+
+		 if(i%RAND_MAX == 0)
+		 {cout << "Used " << i << "rand()'s. Reseeding.." << endl;
+			 srand(0);}
+
+		 double coeff5D = ( (u1-l1)*(u2-l2)*(u3-l3)*(u4-l4)*(u5-l5) ) / i;
+		 double doublePRNG1,doublePRNG2,doublePRNG3,doublePRNG4,doublePRNG5;
+		 double summation = 0;
+
+			 for(int j=1; j<=i; j++)
+			 {
+				doublePRNG1 = prngDouble(0,1);
+				//cout << doublePRNG1 << endl;
+				doublePRNG2 = prngDouble(0,1);
+				//cout << doublePRNG2 << endl;
+				doublePRNG3 = prngDouble(0,1);
+				//cout << doublePRNG3 << endl;
+				doublePRNG4 = prngDouble(0,1);
+				//cout << doublePRNG4 << endl;
+				doublePRNG5 = prngDouble(0,1);
+				//cout << doublePRNG5 << endl << endl << endl;
+
+				summation += pow((doublePRNG1+doublePRNG2+doublePRNG3+doublePRNG4+doublePRNG5),3);
+
+			 }
+
+		 double random5D = coeff5D * summation;
+		 //cout << "Summation = " << setprecision(10) << random5D << endl;
+		 //cout << "Relative error = " << fabs ( 100*(random5D - 18.75) / 18.75 ) << "%" << endl;
+		 of << i << "," << fabs ( (random5D - 18.75) / 18.75 ) << endl;
+
+	 }
+	 of.close();
+	 return 0;
+ }
+
 int main()
 {
-	monteCarlo5D();
+	//double total = totalRands(90000);
+	//cout << "Total rands() used: " << total;
+	cout << "wtf" << endl;
+	double bullshit[3000000000];
+
+	for (int i=1; i< 3000000000; i++)
+	{
+
+		bullshit[i] = prngDouble(0, 1);
+
+		if(i == RAND_MAX)
+		{cout << "RAND_MAX reached @: " << i << endl << endl << endl << bullshit[1] << "wtfwtfwtf" << endl << endl << endl;}
+
+	}
+
+
+
+	cout << "RAND_MAX = " << RAND_MAX << endl;
+
+	for(int i=1; i<RAND_MAX; i++)
+	{
+		if (totalRands(i) >= RAND_MAX)
+		{
+			cout << "Used " << totalRands(i) << "random numbers @ N =" << i << endl;
+			break;
+		}
+	}
+
 	return 0;
 }
